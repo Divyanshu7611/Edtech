6341a303ace0c9105b413e749548bc96ecf9044f
Divyanshu7611 <dshapbss202@gmail.com>
2024-02-03 02:12:32 +0530

    some updations

--- models/User.js
+++ models/User.js
@@ -1,58 +1,69 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     firstName: {
-        type:String,
-        required:true,
-        trim:true,
+      type: String,
+      required: true,
+      trim: true,
     },
-    lastName :{
-        type:String,
-        required:true,
-        trim:true,
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
     },
     email: {
-        type:String,
-        required:true,
-        trim:true,
+      type: String,
+      required: true,
+      trim: true,
     },
     password: {
-        type:String,
-        required:true,
+      type: String,
+      required: true,
     },
     accountType: {
-        type:String,
-        enum:["Admin", "Student", "Instructor"],
-        required:true    
+      type: String,
+      enum: ["Admin", "Student", "Instructor"],
+      required: true,
     },
     additionalDetails: {
-        type:mongoose.Schema.Types.ObjectId,
-        required:true,
-        ref:"Profile",
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Profile",
     },
     courses: [
-        {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"Course",
-        }
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+        index: true,
+      },
     ],
-    image:{
-        type:String,
-        required:true,
+    active: {
+      type: Boolean,
+      default: true,
     },
-    token :{
-        type:String,
+    approved: {
+      type: Boolean,
+      default: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    token: {
+      type: String,
     },
     resetPasswordExpires: {
-        type:Date,
+      type: Date,
     },
     courseProgress: [
-        {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"CourseProgress",
-        }
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "CourseProgress",
+      },
     ],
-
-});
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
